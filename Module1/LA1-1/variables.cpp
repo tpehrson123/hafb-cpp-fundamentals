e23872a3a9381ea1cccdcaa9ca590dc2e972dd1f
Timothy Pehrson <timothypehrson@ad.weber.edu>
2019-12-16 10:37:38 -0700

    size of stuff

---
+++ Module1/LA1-1/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "\nData Type  Bytes"
+        << "\n---------- -----"
+        << "\nint         " << sizeof(int)
+        << "\nchar        " << sizeof(char)
+        << "\nlong        " << sizeof(long)
+        << "\nbool        " << sizeof(bool)
+        << endl;
+    return 0;
+
+}
