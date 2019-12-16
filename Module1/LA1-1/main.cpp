80b5c002285e4e85b83c800ac713548e6235a9b5
Timothy Pehrson <timothypehrson@ad.weber.edu>
2019-12-16 09:41:05 -0700

    namespace using

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
     std::cout << "Now inside VSCode and github " << std::endl;
+    cout << "Another line under namespace std " << endl;
+    cout << "Wildcats" << " WSU" << " Ogden" << endl;
 
     return 0; 
 }
