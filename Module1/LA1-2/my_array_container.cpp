6916975a4ec97b39bb89322dcef6f384597b4d41
Timothy Pehrson <timothypehrson@ad.weber.edu>
2019-12-16 15:23:08 -0700

    qwer

---
+++ Module1/LA1-2/my_array_container.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>    // array container
+using namespace std;
+
+int main()
+{
+
+    // To create arrays with library:
+    // array <type, numOfElements> name
+    array<double, 5> grades {22.1, 6.9, 11.2, 99, 44.3};
+
+    cout << "Size of array: " << grades.size() << endl;
+    cout << "Array empty? " << grades.empty() << endl;
+
+    for (int index = 0; index < grades.size(); index++)
+    {
+
+        cout << grades[index] << endl;
+    }
+    
+    return 0;
+}
