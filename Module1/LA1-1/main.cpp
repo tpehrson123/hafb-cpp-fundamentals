d14eba960acf026a251a9a91327ab0d0a2929df7
Timothy Pehrson <timothypehrson@ad.weber.edu>
2019-12-16 09:53:03 -0700

    Add cin

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 int main()
 {
+    int age;  // int: signed integers
+
+    // cout: for system output
     std::cout << "Now inside VSCode and github " << std::endl;
     cout << "Another line under namespace std " << endl;
     cout << "Wildcats" << " WSU" << " Ogden" << endl;
 
+    cout << "Enter your age?" << endl;
+
+    // cin: for system/user input
+    cin >> age;
+
+    cout << "I see you are: " << age << endl;
     return 0; 
 }
