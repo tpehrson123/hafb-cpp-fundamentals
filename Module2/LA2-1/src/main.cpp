8f85bdd50524b20b6ec7c0d6fa17b575fb41c610
Timothy Pehrson <timothypehrson@ad.weber.edu>
2019-12-17 15:08:22 -0700

    string stuff

--- Module2/LA2-1/src/main.cpp
+++ Module2/LA2-1/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-using namespace std;
+#include <string> //for string objects
 
 /**
  * @brief Shows how to define and use a string object
@@ -10,26 +9,61 @@ using namespace std;
  *
  * @return int 0 means function exited with success
  */
-int main() {
+int main()
+{
   // create a variable of type string
+  std::string name = "Weber Waldo";
 
   // print it out
+  std::cout << name << std::endl;
 
-  // get a string from a user
+  // // get a string from a user
+  // std::cout << "Enter a name: " << std::endl;
+  // // to get strings with spaces, use getline()
+  // getline(std::cin, name);
+  // std::cout << "You entered: " << name << std::endl;
 
   // add two strings together
+  std::string title = "Dr.";
+  std::string formal;
+  formal = title + " " + name;
+  std::cout << "Hi " << formal << std::endl;
 
-  // how long is the string
+  // how long is the string (use either size or length - same thing)
+  // std::cout << "The size of name is " << name.size() << std::endl;
+  // std::cout << "The length of name is " << name.length() << std::endl;
 
-  // use [] to get an individual character
+  // std::cout << "The size of name is " << formal.size() << std::endl;
+  // std::cout << "The length of name is " << formal.length() << std::endl;
+
+  // // use [] to get an individual character
+  // std::cout << "The first char of name is " << name[0] << std::endl;
+  // std::cout << "The third char of name is " << name[2] << std::endl;
 
   // get name and commute information from a user
+  int minutes, miles;
+
+  // std::cout << "Enter your full name: " << std::endl;
+  // // to get strings with spaces, use getline()
+  // getline(std::cin, name);
+  // std::cout << "Enter long is your commute in minutes? " << std::endl;
+  // std::cin >> minutes;
+  // std::cout << "Enter many miles is your commute? " << std::endl;
+  // std::cin >> miles;
+
+  // std::cout << "You entered: " << name << " " << minutes << " " << miles << std::endl;
 
   // get commute information and name from a user
 
   // the string function find
+  std::string story =
+      "Now is the time for all good men to to something"
+      " and to do this and that and the other thing ... like wheres waldo";
+
+  std::cout << "waldo is at location " << story.find("waldo") << std::endl;
 
   // the string function replace
+  std::cout << "New story: " << story.replace(story.find("waldo"), 5, "idiot") << std::endl;
 
   return 0;
 }
